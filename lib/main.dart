59efbc2e92147f28721d1092f115574c127a4165
liyin <shuaiyin.top@foxmail.com>
2023-03-19 13:44:40 +0800

    feat: 添加字体和根据系统更改暗黑模式

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,45 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   bool isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    super.didChangePlatformBrightness();
+    Brightness brightness = WidgetsBinding.instance.window.platformBrightness;
+    setState(() {
+      if (brightness.name == 'light') {
+        isDarkMode = false;
+      } else {
+        isDarkMode = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      theme: ThemeData(
+        fontFamily: 'AaShuYunSong',
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        fontFamily: 'AaShuYunSong',
+        brightness: Brightness.dark,
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Stack(children: [
